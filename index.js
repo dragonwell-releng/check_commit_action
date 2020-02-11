8433cb84df4417006698f6449adc49bddc2bc7ee
Chuan Sheng Lu <chuanshenglu@gmail.com>
2020-02-11 11:40:41 +0800

    Trying to complete the feature

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const github = require('@actions/github');
 try {
     const ref = github.context.ref;
     console.log(`Hello ${ref}!`);
+    if (github.context.eventName === 'pull_request') {
+        console.log("triggered on pull_request!");
+    } else {
+        core.setFailed("Can only be triggered on pull_request, current event=" +
+            github.context.eventName)
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
