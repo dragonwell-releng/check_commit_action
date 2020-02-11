2abf469076945b1a69979db770148b6460f96df9
Jonathan Lu <chuanshenglu@gmail.com>
2020-02-11 15:11:24 +0800

    [Misc] Actually implement the checker
    Summary: using js action
    
    Test Plan: see actions of project luchsh/check_commit_action
    
    Reviewed-by: luchsh
    
    Issue: #1

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const Git = require('git').Git;
+const { execSync } = require('child_process');
+
+// we will use 'git log --pretty=%B######' to show logs,
+// and using special string '########' to seperate revisions.
+const rev_separator = "########";
 
 // check comment of single revision, git metadata lines are not included
-function check_comment(lines) {
+function check_rev_comment(lines) {
+    console.log(lines);
     var title = lines[0];
     // check title
     console.log(">> checking title line:" + title);
@@ -27,70 +32,92 @@ function check_comment(lines) {
     return 0;
 }
 
-// Check comments of last N revisions from 'ref'
-function check_last_n_revisions(nof_revs, ref) {
-    console.log("checking last " + nof_revs + " revisions on " + ref);
-    // parsed comments, each element is from one revision
+// Run git command in sub-process, show the command string and output string;
+function verbose_run(cmd_string) {
+    console.log("Executing command: " + cmd_string);
+    var out_buf = execSync(cmd_string);
+    var out_str = out_buf.toString();
+    console.log("Output:\n\"\"\"\n" + out_str + "\"\"\"");
+    return out_str;
+}
+
+// parse output of 'git log --pretty=raw -l1'
+function parse_raw_git_log(git_raw_log) {
     var comments = [];
-    // expecting repo checked out under working directory
-    var git = new Git(github.context.workspace + '/.git');
-    // retrieve top 1 commit list
-    git.rev_list({ pretty: 'raw', max_count: nof_revs },
-        ref,
-        function(err, rev_output) {
-            if (rev_output == null) {
-                return;
-            }
+    if (git_raw_log == null) {
+        return;
+    }
 
-            //
-            // using git metadata to split revision logs
-            // ----
-            // commit 8433cb84df4417006698f6449adc49bddc2bc7ee
-            // tree 96bac4aa05ae1a819be87031c13325ca0b366cda
-            // parent 329cc65c042491a37a26ddf66ea70aaf47971ea0
-            // author Chuan Sheng Lu <chuanshenglu@gmail.com> 1581392441 +0800
-            // committer Chuan Sheng Lu <chuanshenglu@gmail.com> 1581392908 +0800
-            //
-            //var arr = rev_output.replace(/commit.+tree.+.+committer.+#/g, "####").split("####");
+    var arr = git_raw_log.split(rev_separator);
+    arr.forEach(function(s) {
+        if (s.length > 0) {
+            comments.push(s);
+        }
+    });
+    return comments;
+};
 
-            var arr = rev_output.split("committer");
-            arr.forEach(function(s) {
-                if (s.length > 0) {
-                    s.split('commit').forEach(l => {
-                        if (l.length > 0 && -1 == l.search('author')) {
-                            comm = l.substr(l.search(/\n/) + 1);
-                            comments.push(comm);
-                        }
-                    })
-                }
-            });
-        });
+// Check comments of last N revisions from 'ref'
+function check_last_n_revisions(ref_name, nof_revs) {
+    console.log("checking last " + nof_revs + " revisions on " + ref_name);
+
+    // retrieve top N revisions, using ######## (8 * #) as separator
+    var out_str = verbose_run("git log --pretty=%B" + rev_separator + " -n" + nof_revs + " " + ref_name);
+
+    // parsed comments, each element is from one revision
+    var comments = parse_raw_git_log(out_str);
+
+    if (comments.length <= 0) {
+        core.setFailed("No revision comments is parsed from " + ref_name);
+    }
 
     // check each revision comments
     comments.forEach(com => {
         var cur_comm = [];
         com.split('\n').forEach(line => {
+            line = line.trim();
             if (line.length > 0) {
-                line = line.trim();
                 cur_comm.push(line);
-                console.log(line);
             }
         })
-        if (0 != check_comment(cur_comm)) {
+        if (cur_comm.length > 0 && 0 != check_rev_comment(cur_comm)) {
             core.setFailed("Step check comment failed!")
         }
     });
 }
 
-try {
-    // only trigger for pull_requests
-    if (github.context.eventName === 'pull_request') {
-        console.log("triggered on pull_request!");
-        check_last_n_revisions(1, github.context.ref);
-    } else {
-        core.setFailed("Can only be triggered on pull_request, current event=" +
-            github.context.eventName)
-    }
-} catch (error) {
-    core.setFailed(error.message);
+// help debugging
+function show_envs() {
+    console.log(github.context);
 }
+
+// entry of checker action
+function main() {
+    try {
+        // only trigger for pull_requests
+        if (github.context.eventName === 'pull_request') {
+            // using a unique name for local branch
+            var local_branch = "local_ref_branch_" + Date.now();
+            var remote_ref = github.context.payload.after;
+
+            // fetch pull request to local branch
+            verbose_run("git fetch origin " + remote_ref + ":" + local_branch);
+
+            // run checking
+            check_last_n_revisions(local_branch, 1);
+        } else {
+            core.setFailed("Can only be triggered on pull_request, current event=" +
+                github.context.eventName)
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+// for local testing purpose
+function local_testing() {
+    check_last_n_revisions('local_ref_branch1581603361965', 1);
+}
+
+// local_testing();
+main();
