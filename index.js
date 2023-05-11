4c3da51d61a8e728b3a8fa7f0e1e839e2473fc41
lvfei.lv <lvfei.lv@alibaba-inc.com>
2023-05-11 10:34:02 +0800

    add CDS in list

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const process = require('process');
 const rev_separator = "#".repeat(27);
 
 function tag_of(title) {
-    var known_tags = ['Misc', 'GC', 'MultiTenant', 'JWarmUp', 'RAS', 'JIT', 'JFR', 'Merge', 'Backport', 'Coroutine', 'Wisp', 'EagerAppCDS', 'QuickStart', 'Landing'];
+    var known_tags = ['Misc', 'GC', 'MultiTenant', 'JWarmUp', 'RAS', 'JIT', 'JFR', 'Merge', 'Backport', 'Coroutine', 'Wisp', 'EagerAppCDS', 'QuickStart', 'Landing', 'CDS'];
     return known_tags.find(tag => title.startsWith("[" + tag + "]"));
 }
 
